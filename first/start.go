b9585e92acb22db09f4a4372dad3ab3803220d77
mrrsmth <alxs1dd93@gmail.com>
2024-09-15 16:33:46 +0700

    fix

---
+++ first/start.go
@@ -0,0 +1,100 @@
+package first
+
+// Two Sum
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+// Example 1:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+func TwoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
+
+// Fibonacci Number
+
+// The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+// F(0) = 0, F(1) = 1
+// F(n) = F(n - 1) + F(n - 2), for n > 1.
+// Given n, calculate F(n).
+
+// Example 1:
+// Input: n = 2
+// Output: 1
+// Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+
+// Example 2:
+// Input: n = 3
+// Output: 2
+// Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+
+// Example 3:
+// Input: n = 4
+// Output: 3
+// Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+
+// Constraints:
+// 0 <= n <= 30
+
+func Fib(n int) int {
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+	return Fib(n-1) + Fib(n-2)
+}
+
+// Plus One
+// You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+// Increment the large integer by one and return the resulting array of digits.
+
+// Example 1:
+// Input: digits = [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+// Incrementing by one gives 123 + 1 = 124.
+// Thus, the result should be [1,2,4].
+
+// Example 2:
+// Input: digits = [4,3,2,1]
+// Output: [4,3,2,2]
+// Explanation: The array represents the integer 4321.
+// Incrementing by one gives 4321 + 1 = 4322.
+// Thus, the result should be [4,3,2,2].
+// Example 3:
+// Input: digits = [9]
+// Output: [1,0]
+// Explanation: The array represents the integer 9.
+// Incrementing by one gives 9 + 1 = 10.
+// Thus, the result should be [1,0].
+
+func PlusOne(digits []int) []int {
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] == 9 {
+			digits[i] = 0
+		} else {
+			digits[i]++
+			return digits
+		}
+	}
+
+	return append([]int{1}, digits...)
+}
--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
-	fn "algorithms/fibonacciNumber"
-	po "algorithms/plusOne"
-	ts "algorithms/twoSum"
+	f "algorithms/first"
+	tp "algorithms/twoPointers"
+
 	"fmt"
 )
 
 func main() {
 	fmt.Println("Hello algorithm!")
 
-	fmt.Println(ts.TwoSum([]int{3, 2, 4}, 6))
+	// First
+	fmt.Println(f.TwoSum([]int{3, 2, 4}, 6))
+	fmt.Println(f.Fib(5))
+	fmt.Println(f.PlusOne([]int{4, 3, 2, 1}))
 
-	fmt.Println(fn.Fib(5))
-
-	fmt.Println(po.PlusOne([]int{4, 3, 2, 1}))
+	//TwoPointers
+	fmt.Println(tp.Init())
 }
