70c8d0155db162eed7e1a0a82e0ffcc7ecd1d91a
iWill <alxs1dd93@gmail.com>
2025-05-18 05:36:41 +0700

    isPalindrome, Find the Index of the First Occurrence in a String

--- ts-algorithms/src/two-pointers/two-pointers.ts
+++ ts-algorithms/src/two-pointers/two-pointers.ts
@@ -7,3 +7,40 @@ export const containsDuplicate = (nums: number[]): boolean => {
     }
     return false;
 };
+
+export const isPalindrome = (s: string): boolean => {
+    const clean = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+    let left = 0;
+    let right = clean.length - 1;
+
+    while (left < right) {
+        if (clean[left] !== clean[right]) return false;
+        left++;
+        right--;
+    }
+    return true;
+};
+
+export const strStr = (haystack: string, needle: string): number => {
+    if (needle.length === 0) return 0;
+
+    const haystackLen = haystack.length;
+    const needleLen = needle.length;
+
+    const maxStartIndex = haystackLen - needleLen;
+
+    for (let i = 0; i <= maxStartIndex; i++) {
+        let j = 0;
+
+        while (j < needleLen && haystack[i + j] === needle[j]) {
+            j++;
+        }
+
+        if (j === needleLen) {
+            return i;
+        }
+    }
+
+    return -1;
+};
