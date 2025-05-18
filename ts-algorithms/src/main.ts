35dae66f6d629f7bb83fc678e42f56cd554cc0eb
iWill <alxs1dd93@gmail.com>
2025-05-18 17:57:43 +0700

    fix

--- ts-algorithms/src/main.ts
+++ ts-algorithms/src/main.ts
@@ -1,10 +1,16 @@
-import { longestOnes, longestSubarray } from './sliding-window/sliding-window.js';
+import { containsNearbyDuplicate, longestOnes, longestSubarray } from './sliding-window/sliding-window.js';
 import { containsDuplicate, isPalindrome, strStr } from './two-pointers/two-pointers.js';
 import { sum } from './utils/math.js';
 
+// math
 sum(2, 3);
-longestSubarray([1, 0, 1, 1, 1]);
-longestOnes([0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], 3);
+
+// Two pointers
 containsDuplicate([1,2,3,]);
 isPalindrome("aaaa");
 strStr('abc', 'c');
+
+// Sliding window
+longestSubarray([1, 0, 1, 1, 1]);
+longestOnes([0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], 3);
+containsNearbyDuplicate([1,2,3,1], 3);
