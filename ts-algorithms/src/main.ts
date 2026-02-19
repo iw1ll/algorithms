afe165ed9eec6326e4eb10cdf35cc4b02d0ebde0
iWill <alxs1dd93@gmail.com>
2026-02-19 21:43:04 +0700

    fix

--- ts-algorithms/src/main.ts
+++ ts-algorithms/src/main.ts
@@ -2,27 +2,27 @@ import { testList } from './linked-list/design-linked-list.js';
 import { ListNode, middleNode } from './linked-list/middle-off-the-linked-list.js';
 import { reverseList } from './linked-list/reverse-linked-list.js';
 import { containsNearbyDuplicate, countGoodSubstrings, longestOnes, longestSubarray, minimumDifference, minimumRecolors } from './sliding-window/sliding-window.js';
-import { containsDuplicate, isPalindrome, reverseString, strStr, twoSum } from './two-pointers/two-pointers.js';
+import { containsDuplicate, isPalindrome, reverseString, sortedSquares, strStr, twoSum } from './two-pointers/two-pointers.js';
 import { sum } from './utils/math.js';
 
 /** Math */
 sum(2, 3);
 
 /** Two pointers */
-containsDuplicate([1,2,3,]);
+containsDuplicate([1, 2, 3]);
 isPalindrome("A man, a plan, a canal: Panama");
 strStr('abc', 'c');
 reverseString(["h","e","l","l","o"]);
-twoSum([2,3,4], 6);
-
+twoSum([2, 3, 4], 6);
+sortedSquares([-4 ,-1 ,0 ,3 , 10]);
 
 /** Sliding window  */
 longestSubarray([1, 0, 1, 1, 1]);
-longestOnes([0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], 3);
-containsNearbyDuplicate([1,2,3,1], 3);
+longestOnes([0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1], 3);
+containsNearbyDuplicate([1, 2, 3, 1], 3);
 countGoodSubstrings('aababcabc');
 minimumRecolors('WBWBBBW', 2);
-minimumDifference([9,4,1,7], 2);
+minimumDifference([9, 4, 1, 7], 2);
 
 /** Linked List */
 const list = new ListNode(1, new ListNode(2, new ListNode(3, null)));
