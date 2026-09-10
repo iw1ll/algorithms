ae041d0aa5324289a02fc78d21c2ef97cccf1e72
iWill <alxs1dd93@gmail.com>
2026-09-10 16:23:46 +0700

    simplifyPath, RecentCounter

--- ts-algorithms/src/main.ts
+++ ts-algorithms/src/main.ts
@@ -4,7 +4,7 @@ import { ListNode } from './linked-list/middle-off-the-linked-list.js';
 import { reverseList } from './linked-list/reverse-linked-list.js';
 import { LRUCache } from './LRU/lru.js';
 import { containsNearbyDuplicate, countGoodSubstrings, longestOnes, longestSubarray, minimumDifference, minimumRecolors, totalFruit } from './sliding-window/sliding-window.js';
-import { isValid, removeDuplicatesS } from './stack-queue/stack-queue.js';
+import { isValid, removeDuplicatesS, simplifyPath } from './stack-queue/stack-queue.js';
 import { containsDuplicate, isPalindromeStr, maxArea, removeDuplicates, reverseString, sortedSquares, strStr, twoSum, } from './two-pointers/two-pointers.js';
 import { sum } from './utils/math.js';
 
@@ -52,6 +52,8 @@ hashMap.put(1, 1)
 const lru = new LRUCache(3);
 lru.put(1, 1);
 
-/** LRU */
+/** Stack queue */
 isValid('()[]{}');
-console.log(removeDuplicatesS('abbaca'))
+removeDuplicatesS('abbaca');
+simplifyPath("/home/user/Documents/../Pictures");
+console.log()
--- ts-algorithms/src/stack-queue/stack-queue.ts
+++ ts-algorithms/src/stack-queue/stack-queue.ts
@@ -45,3 +45,41 @@ export function removeDuplicatesS(s: string): string {
     
     return stack.join('');
 };
+
+export function simplifyPath(path: string): string {
+    const normalPath = path.split('/');
+    const stack: string[] = [];
+
+    for (const p of normalPath) {
+        if (p === '' || p === '.') {
+            continue;
+        }
+        if (p !== '..') {
+            stack.push(p);
+        } else {
+            if(stack.length) {
+                stack.pop()
+            }
+        }
+    }
+
+    return '/' + stack.join('/');
+};
+
+export class RecentCounter {
+    stack: number[];
+
+    constructor() {
+        this.stack = [];
+    }
+
+    ping(t: number): number {
+        this.stack.push(t);
+
+        while (this.stack[0] < t - 3000) {
+            this.stack.shift();
+        }
+
+        return this.stack.length;
+    }
+}
